af6874d1c74cbbb31def3c9f872a04ac9a0c1490
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2020-06-20 20:21:33 +0200

    Fixed styles and field size.

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # 2048 Game
 
-Simplified version of real 2048 game made with JS.
-
-![2048](https://imgur.com/ub1xXhO.png)
+Simplified version of real 2048 game made with JS. *Version 1.0.0.*
 
 ### How to run?
 
 1. Clone this repository.
 2. Open `index.html` in your browser.
+3. Use `WASD` keys to move cells.
+
+![2048](https://imgur.com/xLfulJb.png)
+
+### To-do list
+
+- Add animation for cell.
+- Add "New game" button.
--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
-let WON = false;
-let GENERATE_RANDOM = false;
-let SCORE = 0;
-
-// Initiate field for game
 const HEIGHT = 4;
-const WIDTH = 4;
+const WIDTH = HEIGHT;
 const FIELD = new Array(HEIGHT).fill().map(() => new Array(WIDTH).fill(0));
+const COVER = {
+	win: {
+		color: "rgba(255, 217, 0, .4)",
+		text: "You win!",
+	},
+	lose: {
+		color: "rgba(255, 255, 255, .6)",
+		text: "Game Over",
+	},
+};
+
+let WON = false;
+let GENERATE_NEXT = false;
+let SCORE = 0;
 
 // Additional function to remove zero values
 function removeZeros(array, position = "l") {
@@ -18,7 +27,7 @@ function removeZeros(array, position = "l") {
 
 	// Check if smth has changed
 	if (JSON.stringify(prev) != JSON.stringify(array)) {
-		GENERATE_RANDOM = true;
+		GENERATE_NEXT = true;
 	}
 
 	return array;
@@ -27,7 +36,10 @@ function removeZeros(array, position = "l") {
 // Move all numbers to down side
 function moveDown() {
 	for (let i = 0; i < WIDTH; i++) {
-		column = removeZeros(FIELD.map((e) => e[i]), "r");
+		column = removeZeros(
+			FIELD.map((e) => e[i]),
+			"r"
+		);
 		column = slideRight(column);
 		for (let j = 0; j < HEIGHT; j++) {
 			FIELD[j][i] = column[j];
@@ -52,12 +64,12 @@ function slideLeft(array) {
 		if (array[j] === array[j + 1] && array[j] !== 0) {
 			SCORE += array[j] + array[j + 1];
 			array[j] = array[j] + array[j + 1];
-			if (array[j] === 2048) {
+			if (array[j] >= 2048) {
 				WON = true;
 			}
 			array[j + 1] = 0;
 			array = removeZeros(array);
-			GENERATE_RANDOM = true;
+			GENERATE_NEXT = true;
 		}
 	}
 	return array;
@@ -69,12 +81,12 @@ function slideRight(array) {
 		if (array[j] === array[j - 1] && array[j] !== 0) {
 			SCORE += array[j] + array[j - 1];
 			array[j] = array[j] + array[j - 1];
-			if (array[j] === 2048) {
+			if (array[j] >= 2048) {
 				WON = true;
 			}
 			array[j - 1] = 0;
 			array = removeZeros(array, "r");
-			GENERATE_RANDOM = true;
+			GENERATE_NEXT = true;
 		}
 	}
 	return array;
@@ -100,15 +112,11 @@ function moveRight() {
 function checkLose() {
 	for (let i = 0; i < HEIGHT; i++) {
 		for (let j = 0; j < WIDTH; j++) {
-			if (FIELD[i][j] === 0) {
-				return false;
-			}
-
-			if (i < HEIGHT - 1 && FIELD[i][j] == FIELD[i + 1][j]) {
-				return false;
-			}
-
-			if (j < WIDTH - 1 && FIELD[i][j] == FIELD[i][j + 1]) {
+			if (
+				FIELD[i][j] === 0 ||
+				(i < HEIGHT - 1 && FIELD[i][j] == FIELD[i + 1][j]) ||
+				(j < WIDTH - 1 && FIELD[i][j] == FIELD[i][j + 1])
+			) {
 				return false;
 			}
 		}
@@ -117,18 +125,17 @@ function checkLose() {
 }
 
 // Change background and write end message
-function endGame(background, text) {
-	document.getElementById("game-container").style.background = background;
+function setWrapper(win = true, background, text, btn = "block") {
 	document.getElementById("game-dialog").innerHTML = text;
-	if (text === "Game Over") {
-		document.getElementById("game-dialog").style.color = "#776e65";
-	}
+	document.getElementById("game-reset").style.display = btn;
+	document.getElementById("game-container").style.background = background;
+	document.getElementById("game-dialog").style.color = win ? "fff" : "#776e65";
 	document.removeEventListener("keydown", move);
 }
 
 // Main key handler
 function move(e) {
-	GENERATE_RANDOM = false;
+	GENERATE_NEXT = false;
 
 	if (e.key == "a") moveLeft();
 	if (e.key == "d") moveRight();
@@ -136,36 +143,30 @@ function move(e) {
 	if (e.key == "w") moveUp();
 
 	if ("adsw".includes(e.key)) {
-		if (GENERATE_RANDOM) {
-			newRandomCell();
-		}
+		if (GENERATE_NEXT) newRandomCell();
 		refreshUI();
-		if (WON) {
-			endGame("rgba(255, 217, 0, 0.4)", "You win!");
-		}
-		if (checkLose()) {
-			endGame("rgba(255, 255, 255, .6)", "Game Over");
-		}
+		if (WON) setWrapper(true, COVER.win.color, COVER.win.text);
+		if (checkLose()) setWrapper(false, COVER.lose.color, COVER.lose.text);
 	}
 }
 
 // Add new random cell on the field
 function newRandomCell() {
-	const [i, j] = getRandomCoordinates();
-	FIELD[i][j] = 2;
-}
-
-// Get coordinates of random cell on the field by finding zeros cell
-function getRandomCoordinates() {
-	let free = [];
-	for (let i = 0; i < HEIGHT; i++) {
-		for (let j = 0; j < WIDTH; j++) {
-			if (FIELD[i][j] == 0) {
-				free.push([i, j]);
+	// Get coordinates of random cell on the field by finding zeros cell
+	function getRandomCoordinates() {
+		let free = [];
+		for (let i = 0; i < HEIGHT; i++) {
+			for (let j = 0; j < WIDTH; j++) {
+				if (FIELD[i][j] === 0) {
+					free.push([i, j]);
+				}
 			}
 		}
+		return free[Math.floor(Math.random() * free.length)] || [0, 0];
 	}
-	return free[Math.floor(Math.random() * free.length)] || [0, 0];
+
+	const [i, j] = getRandomCoordinates();
+	FIELD[i][j] = 2;
 }
 
 // Refresh UI by redrawing field
@@ -173,34 +174,61 @@ function refreshUI() {
 	document.getElementById("game-score").innerHTML = SCORE;
 	for (let i = 0; i < HEIGHT; i++) {
 		for (let j = 0; j < WIDTH; j++) {
-			const cell = document.getElementById(i * 4 + j + 1);
+			const cell = document.getElementById(i * HEIGHT + j + 1);
 			cell.className = "";
 			cell.innerHTML = "";
 			cell.classList.add(`_${FIELD[i][j]}`, "c");
-			if (FIELD[i][j] != 0) {
+			if (FIELD[i][j] !== 0) {
 				cell.innerHTML = FIELD[i][j];
 			}
 		}
 	}
 }
 
-// Generate div blocks
-function createCells() {
-	const f = document.getElementById("field");
-	for (let i = 0; i < HEIGHT * WIDTH; i++) {
-		const cell = document.createElement("div");
-		cell.setAttribute("id", i + 1);
-		f.appendChild(cell);
+// Resets game score and cells
+function resetGame() {
+	SCORE = 0;
+	WON = false;
+	GENERATE_NEXT = true;
+	setWrapper(true, "", "", "none");
+	document.addEventListener("keydown", move);
+
+	// Reset field
+	for (let i = 0; i < HEIGHT; i++) {
+		for (let j = 0; j < WIDTH; j++) {
+			FIELD[i][j] = 0;
+		}
 	}
+
+	newRandomCell();
+	newRandomCell();
+	refreshUI();
+}
+
+// Insert nodes in DOM
+function initiateCells() {
+	const field = document.getElementById("field");
+	for (let i = 0; i < WIDTH * HEIGHT; i++) {
+		const cell = document.createElement("div");
+		cell.className = "c";
+		cell.id = i + 1;
+		field.appendChild(cell);
+	}
+
+	// Insert style for class .c into css rules
+	const style = document.createElement("style");
+	document.head.appendChild(style);
+	style.sheet.insertRule(
+		`.c {
+			width: calc(520px / ${WIDTH} - 20px);
+			height: calc(520px / ${HEIGHT} - 20px); 
+			line-height: calc(520px / ${HEIGHT} - 20px);
+		}`
+	);
 }
 
 document.addEventListener("keydown", move);
+document.getElementById("game-reset").addEventListener("click", resetGame);
 
-// Game begins from two random cells
-createCells();
-newRandomCell();
-newRandomCell();
-refreshUI();
-
-// TODO: Change font size
-// TODO: Add step back
+initiateCells();
+resetGame();
